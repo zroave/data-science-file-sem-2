c802755d1d171c374c67d378659d3320c036f414
Aftab <ar5849119@gmail.com>
2026-04-22 16:59:21 +0530

    .4

---
+++ largestAmongThree.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+if a >= b and a >= c:
+    print("Largest:", a)
+elif b >= a and b >= c:
+    print("Largest:", b)
+else:
+    print("Largest:", c)
